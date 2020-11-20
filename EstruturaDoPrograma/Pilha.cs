5b81ab3468e0deb080292da66dc95a421906c0a6
Jailsom Nogueira <jailsom.nogueira@gmail.com>
2020-11-20 13:40:57 -0300

    intro to classes

---
+++ EstruturaDoPrograma/Pilha.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EstruturaDoPrograma
+{
+  class Pilha
+  {
+    Posicao primeiro;
+
+    public void Empilha(object item)
+    {
+      primeiro = new Posicao(primeiro, item);
+    }
+
+    public object Desempilha()
+    {
+      if (primeiro == null)
+      {
+        throw new InvalidOperationException("A pilha está vazia");
+      }
+
+      object resultado = primeiro.item;
+      primeiro = primeiro.proximo;
+      return resultado;
+    }
+
+    class Posicao
+    {
+      public Posicao proximo;
+      public object item;
+      public Posicao(Posicao proximo, object item)
+      {
+        this.proximo = proximo;
+        this.item = item;
+      }
+
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
